813b7d1a452917c93f0594b2ec18055395fdeb13
Plotnikova Mariya <m.buryakova@yandex.ru>
2021-02-24 11:30:30 +0500

    changed manager

--- src/main/java/runetology/manager/PostManager.java
+++ src/main/java/runetology/manager/PostManager.java
@@ -1,4 +1,13 @@
 package runetology.manager;
 
+import runetology.domain.Post;
+
 public class PostManager {
+        public Post[] searchPostName(int ownerId, String domain, String query, int count, int offset) {
+
+        return null;
+        }
+        public void deletePostName(int ownerId, int postId) {
+            return;
+        }
 }
